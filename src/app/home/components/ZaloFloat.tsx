7d07a65068bd26b65681d7e33b874e9558f8b248
xmrquangx <xmrquangx@gmail.com>
2026-03-26 06:34:08 +0700

    fix: ZaloFloat use full zalo URL from options

--- src/app/home/components/ZaloFloat.tsx
+++ src/app/home/components/ZaloFloat.tsx
@@ -16,7 +16,7 @@ export default function ZaloFloat() {
 
   return (
     <a
-      href={`https://zalo.me/${zalo.replace(/\s/g, '')}`}
+      href={`${zalo.replace(/\s/g, '')}`}
       target="_blank"
       rel="noopener noreferrer"
       className="zalo-float"
@@ -24,7 +24,7 @@ export default function ZaloFloat() {
       title="Chat Zalo với WebAgencyVN"
     >
       <svg className="w-7 h-7 text-white" viewBox="0 0 24 24" fill="currentColor">
-        <path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm5.562 8.248l-1.97 9.03c-.145.658-.537.818-1.084.508l-3-2.21-1.447 1.394c-.16.16-.295.295-.605.295l.213-3.053 5.56-5.023c.242-.213-.054-.333-.373-.12L6.26 14.167l-2.95-.924c-.642-.204-.654-.642.136-.953l11.527-4.445c.535-.194 1.003.131.589.403z"/>
+        <path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm5.562 8.248l-1.97 9.03c-.145.658-.537.818-1.084.508l-3-2.21-1.447 1.394c-.16.16-.295.295-.605.295l.213-3.053 5.56-5.023c.242-.213-.054-.333-.373-.12L6.26 14.167l-2.95-.924c-.642-.204-.654-.642.136-.953l11.527-4.445c.535-.194 1.003.131.589.403z" />
       </svg>
       <span className="sr-only">Chat Zalo</span>
     </a>
